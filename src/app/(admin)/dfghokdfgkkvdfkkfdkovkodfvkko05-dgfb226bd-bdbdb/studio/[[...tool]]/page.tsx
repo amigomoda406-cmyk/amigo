bc0cf7fde69d98c65b41aa29ab43c3bdb17d90e5
Amigo Moda <amigomoda406@gmail.com>
2026-08-30 20:38:55 +0100

    fix: mount sanity studio in correct secure path

--- src/app/(admin)/dfghokdfgkkvdfkkfdkovkodfvkko05-dgfb226bd-bdbdb/studio/[[...tool]]/page.tsx
+++ src/app/(admin)/dfghokdfgkkvdfkkfdkovkodfvkko05-dgfb226bd-bdbdb/studio/[[...tool]]/page.tsx
@@ -1,8 +1,7 @@
-// src/app/cms-qx7t3m9n5k2w8p6r4jzv/[[...tool]]/page.tsx
 'use client';
 
 import { NextStudio } from 'next-sanity/studio';
-import config from '../../../../sanity.config';
+import config from '../../../../../../sanity.config';
 
 export default function StudioPage() {
   return <NextStudio config={config} />;
